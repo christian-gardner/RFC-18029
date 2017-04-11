ddd1dc18072d5cfb7ed39408203d798cfd42f032
christian-gardner <christian.gardner@safeguardproperties.com>
2017-04-11 11:00:03 -0400

    Update rdm_rfc_18029_SQA_TD_DATA_BATCH_IDX.sql

--- rdm_rfc_18029_SQA_TD_DATA_BATCH_IDX.sql
+++ rdm_rfc_18029_SQA_TD_DATA_BATCH_IDX.sql
@@ -1,7 +1,24 @@
 
-drop index RDM.SQA_TD_DATA_BATCH;
+begin 
 
 
-CREATE INDEX RDM.SQA_TD_DATA_BATCH ON RDM.SQA_TD_DATA ( CONTRACTOR, REPORT_SEGMENT, COMPLETED_DT);
+    begin
 
+    execute immediate 'drop index RDM.SQA_TD_DATA_BATCH';
+
+    exception 
+            when others then
+            null;
+    end;
+
+
+
+
+ EXECUTE IMMEDIATE 'CREATE INDEX RDM.SQA_TD_DATA_BATCH ON RDM.SQA_TD_DATA ( CONTRACTOR, REPORT_SEGMENT, COMPLETED_DT)';
+
+EXCEPTION 
+      WHEN OTHERS THEN
+      NULL;
+END;
+/
 
